132d9493009ad9490f7b544767d076f69e6b262c
jose <jmromerogarcia1996@gmail.com>
2026-06-08 12:52:33 +0200

    index.html
    Responsive movil y tablet

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -7,3 +7,17 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 });
+
+//Menú toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
+
+navLinks.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('open');
+    });
+});
