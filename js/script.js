3bc5f498ff0bee14dec5c8c06b73f2fd9005f22b
jose <jmromerogarcia1996@gmail.com>
2026-06-09 16:35:49 +0200

    contacto
    Creado el map y su script en la seccion contacto

--- js/script.js
+++ js/script.js
@@ -46,3 +46,40 @@ document.addEventListener('noticiasListas', () => {
     elements = document.querySelectorAll('.titulo, .card-servicio, .card-proyecto, .card-noticia');
     mostrarElements();
 })
+//MAPA -- PAGINA CONTACTO
+//Pedir al navegador que nos localize
+let options = {
+    //Hacemos que calcule la posición mas exacta posible
+    enableHighAccuracy : true,
+    timeout: 5000,
+    maximunAge: 0
+}
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+        success,
+        error,
+        options
+    );
+}else{
+    alert('Los servicios de geolocalización no estan disponibles');
+}
+
+function success(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    let map = L.map('map',{
+        center: [latitude, longitude],
+        zoom: 14
+    });
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution:'DevSolutions'}).addTo(map);
+
+    let control = L.Routing.control({
+        waypoints:[
+            L.latLng(latitude, longitude),
+            L.latLng(37.392524, -6.002147)
+        ],
+        language: 'es'
+    }).addTo(map);
+}
+function error(){}
